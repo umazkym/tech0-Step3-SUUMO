f21f1bf685cba049852f1b81d2f28042c538fb9b
Haruto Yamazaki <tnszk22@gmail.com>
2024-01-20 02:24:07 +0900

    ログイン機能追加

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import sqlite3
+import pandas as pd
 
 header_left, header_right = st.columns([4, 1])
 with header_left:
@@ -8,10 +10,6 @@ with header_right:
     st.button("ログアウト", type="primary")
 
 st.header('', divider='blue')
-
-# html = "<h1>こんにちは</h1>"
-# st.components.v1.html("<center>" + html + "</center>")
-
 
 input_left, input, input_right = st.columns([1.5, 4, 1.5])
 
@@ -37,10 +35,30 @@ with input:
         passwd = st.text_input('パスワード', type='password', max_chars=20)
         st.button("ログイン",type="primary")
 
-
 with input:
     if selected == "新規作成":
         st.subheader("初めまして")
         userid = st.text_input('ログインID',max_chars=10)
         passwd = st.text_input('パスワード', type='password', max_chars=20)
-        st.button("ログイン", type="primary")
+        if st.button("新規作成", type="primary"):
+            db_path = 'property.db'
+            table_name = 'authentication'   #認証
+            conn = sqlite3.connect(db_path)
+
+            # テーブルの作成（存在しない場合のみ）
+            conn.cursor().execute('''
+                CREATE TABLE IF NOT EXISTS authentication (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL
+                )
+            ''')
+
+            # データベースへの挿入処理
+            conn.cursor().execute("INSERT INTO authentication (username, password) VALUES (?, ?)", (userid, passwd))
+            conn.commit()
+
+            st.success(f'{userid}さんのユーザーが正常に作成されました！')
+
+            # 接続をクローズ
+            conn.close()
